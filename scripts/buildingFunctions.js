906f5de78f59d9fc0cfba0b07487e045b7b70725
DiogoZ <diogozang2@gmail.com>
2021-07-20 22:01:07 -0300

    item list updated and it is functional

---
+++ scripts/buildingFunctions.js
@@ -0,0 +1,34 @@
+
+//CONSTANTES UTILIZADAS
+const qs = (element)=>document.querySelector(element);
+
+//INSERIR CADA HAMBURGUER (IMAGEM, NOME, PREÇO, ID) NO MODAL
+burgerJSON.map((burger)=>{
+    let modal = qs('.item-modal').cloneNode(true);
+
+    modal.querySelector('.modal-image').innerHTML = `<img src ='${burger.src}'/>`;
+    modal.querySelector('.modal-title').innerHTML = burger.name;
+    modal.querySelector('.modal-price').innerHTML = `R$ ${(burger.pricing).toFixed(2)}`;
+    modal.querySelector('.add').setAttribute('data-burger', burger.id);
+
+    qs('.display').append( modal );
+});
+
+
+// FUNÇÃO PARA ABRIR E FECHAR VISUALIZAÇÃO DA SACOLA
+function toggleBag() {
+    qs('.cart').classList.toggle('hidden');
+}
+
+
+
+//ARRAY RESPONSÁVEL POR ARMAZENAR OS ÍTENS ADICIONADOS À SACOLA DE COMPRA
+let bagArray = [];
+
+
+//FUNÇÃO QUE ADICIONA OS ÍTENS AO ARRAY DA SACOLA DE COMPRA
+function add_toCart(id){
+    let burgId = id.getAttribute('data-burger');
+
+    bagArray.push(burgerJSON[burgId]);
+}
---
+++ scripts/burgerJSON.js
@@ -0,0 +1,52 @@
+let burgerJSON = [
+    {
+        id:1,
+        src:"../images/burger450/burger1.png", 
+        name:"Double n' Cheese",
+        pricing:20.80,
+    },
+    {
+        id:2,
+        src:"../images/burger450/burger2.png", 
+        name:"Stick",
+        pricing:21.00,
+    },
+    {
+        id:3,
+        src:"../images/burger450/burger3.png", 
+        name:"McLovin",
+        pricing:14.90,
+    },
+    {
+        id:4,
+        src:"../images/burger450/burger4.png", 
+        name:"Old Jenkins",
+        pricing:23.50,
+    },
+    {
+        id:5,
+        src:"../images/burger450/burger5.png", 
+        name:"Mr. Burns",
+        pricing:17.00,
+    },
+    {
+        id:6,
+        src:"../images/burger450/burger6.png", 
+        name:"Half Ralph",
+        pricing:12.10,
+    },
+    {
+        id:7,
+        src:"../images/burger450/burger7.png", 
+        name:"Light Garden",
+        pricing:14.75,
+    },
+    {
+        id:8,
+        src:"../images/burger450/burger8.png", 
+        name:"Monster",
+        pricing:26,
+    }
+];
+
+
